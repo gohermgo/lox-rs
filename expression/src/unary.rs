c480910ccb7e5f97d930a9120b718fbc834219ef
gohermgo <gohnherman@gmail.com>
2024-11-27 17:34:36 +0100

    expanded unit test coverage

--- expression/src/unary.rs
+++ expression/src/unary.rs
@@ -38,13 +38,13 @@ impl UnaryNodeOperator for UnaryOperator {
     }
 }
 #[derive(Debug)]
-pub struct UnaryExpression<O, Output>
+pub struct UnaryExpression<T, Output>
 where
-    O: Expression,
+    T: Expression,
     Output: Expression,
 {
-    operand: O,
-    operator: Box<dyn UnaryNodeOperator<A = O, Output = Output>>,
+    operand: T,
+    operator: Box<dyn UnaryNodeOperator<A = T, Output = Output>>,
 }
 impl From<UnaryExpression<Node, Node>> for Node {
     fn from(value: UnaryExpression<Node, Node>) -> Self {
@@ -68,17 +68,47 @@ where
         })
     }
 }
-// impl<O, Output> Expression for UnaryExpression<&O, Output>
-// where
-//     O: Expression + fmt::Debug,
-//     Output: Expression + fmt::Debug,
-// {
-//     type Output = Output;
-//     fn eval(&self) -> Option<Self::Output> {
-//         self.operand.eval().and_then(|a| {
-//             let res = self.operator.identity()(a);
-//             trace!("Evaluated {self:?} to {res:?}");
-//             res
-//         })
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod neg {
+        use super::*;
+        #[test]
+        fn number() {
+            assert_eq!(
+                UnaryOperator::Neg.express(1.0.into()).eval(),
+                Some(Node::from(-1.0))
+            )
+        }
+        #[test]
+        fn string() {
+            assert!(UnaryOperator::Neg.express("test".into()).eval().is_none())
+        }
+        #[test]
+        fn boolean() {
+            assert!(UnaryOperator::Neg.express(true.into()).eval().is_none())
+        }
+    }
+    mod not {
+        use super::*;
+        #[test]
+        fn true_to_false() {
+            assert!(UnaryOperator::Not
+                .express(true.into())
+                .eval()
+                .as_ref()
+                .and_then(Node::as_bool)
+                .is_some_and(|v| !v));
+        }
+        #[test]
+        fn false_to_true() {
+            assert!(UnaryOperator::Not
+                .express(false.into())
+                .eval()
+                .as_ref()
+                .and_then(Node::as_bool)
+                .is_some_and(std::convert::identity));
+        }
+    }
+}
